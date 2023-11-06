e38a4a1b2ea256d5bdd90013ad786b19f3a66952
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-11-07 03:44:19 +0700

    fix web sign-out callback

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,10 +5,8 @@ import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
 import { signOut } from "next-auth/react";
-import useOrigin from "../origin";
 
 export default function SideNav() {
-  const origin = useOrigin();
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -23,7 +21,7 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <button
-          onClick={() => signOut({ redirect: true, callbackUrl: `${origin}` })}
+          onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
           className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
         >
           <PowerIcon className="w-6" />
