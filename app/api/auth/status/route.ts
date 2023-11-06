23fb7394b4979a1025c32c84fb3541fb35faa5e8
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-11-07 03:36:14 +0700

    Redirect user to dashboard if user hit login page

--- app/api/auth/status/route.ts
+++ app/api/auth/status/route.ts
@@ -2,5 +2,5 @@ import { auth } from "@/auth.api";
 import { ApiResponse } from "@/app/api/api-definitions";
 
 export const GET = auth((req) => {
-  return Response.json({ user: req.auth?.user });
+  return Response.json({ isLoggedIn: !!req.auth?.user });
 }) as ApiResponse;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    fetch(`/api/auth/status`)
+      .then((response) => response.json())
+      .then(({ isLoggedIn }) => {
+        if (isLoggedIn) replace("/dashboard");
+      });
+  }, [replace]);
+
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
